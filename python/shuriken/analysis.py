35d55b563676a5e3d4b85ecb4cccd1c1205fab37
Chris Timperley <christimperley@googlemail.com>
2018-07-02 18:46:14 -0400

    Added basic Travis configuration file (#1)
    * added basic travis file
    
    * bug fix: bad directory given to mypy
    
    * added missing type annotation
    
    * added build status flag
    
    * tidied up Python module structure
    
    * auto-deploy to PyPI
    
    * bug fix: added missing imports

--- python/shuriken/analysis.py
+++ python/shuriken/analysis.py
@@ -1,9 +1,31 @@
-from typing import List
+__all__ = ['Analysis']
 
-from .location import FileLocation, FileLocationRange, FileLocationRangeSet
+from typing import List, Optional
+
+from bugzoo.core.bug import Bug as Snapshot
+from bugzoo.core.container import Container
+from bugzoo.client import Client as BugZooClient
+
+from .core import FileLocation, FileLocationRange, FileLocationRangeSet
+from .loops import find_loops
 
 
 class Analysis(object):
+    @staticmethod
+    def build(client_bugzoo: BugZooClient,
+              snapshot: Snapshot,
+              files: List[str]
+              ) -> 'Analysis':
+        # FIXME assumes binaries are already present in container
+        container = None  # type: Optional[Container]
+        try:
+            container = client_bugzoo.containers.provision(snapshot)
+            loop_bodies = find_loops(client_bugzoo, snapshot, files, container)
+            return Analysis(loop_bodies, [])
+        finally:
+            if container:
+                del client_bugzoo.containers[container.uid]
+
     def __init__(self,
                  loop_bodies: List[FileLocationRange],
                  function_bodies: List[FileLocationRange]
---
+++ python/shuriken/loops.py
@@ -0,0 +1,35 @@
+from typing import List, Dict
+import json
+
+from bugzoo.client import Client as BugZooClient
+from bugzoo.core.bug import Bug as Snapshot
+from bugzoo.core.container import Container
+
+from .core import FileLocationRange
+from .exceptions import BondException
+
+
+def find_loops(client_bugzoo: BugZooClient,
+               snapshot: Snapshot,
+               files: List[str],
+               container: Container
+               ) -> List[FileLocationRange]:
+    loop_bodies = []  # type: List[FileLocationRange]
+
+    out_fn = "loops.json"
+    cmd = "shuriken-loop-finder {}".format(' '.join(files))
+    workdir = "/ros_ws"
+    outcome = client_bugzoo.containers.exec(container, cmd, context=workdir)
+
+    if outcome.code != 0:
+        msg = "loop finder exited with non-zero code: {}"
+        msg = msg.format(outcome.code)
+        raise BondException(msg)
+
+    output = client_bugzoo.files.read(container, out_fn)
+    jsn = json.loads(output)  # type: List[Dict[str, str]]
+    for loop_info in jsn:
+        loc = FileLocationRange.from_string(loop_info['body'])
+        loop_bodies.append(loc)
+
+    return loop_bodies
