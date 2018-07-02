e5d859c0ab32f0f7bebba87a1dd8a53cd6ed39f2
ChrisTimperley <christimperley@googlemail.com>
2018-07-01 22:07:06 -0400

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import glob
+import setuptools
+
+path = os.path.join(os.path.dirname(__file__), 'python/bond/version.py')
+with open(path, 'r') as f:
+    exec(f.read())
+
+setuptools.setup(
+    name='bond',
+    version=__version__,
+    description='A simple static analyer for C++',
+    long_description='TBA',
+    author='Chris Timperley',
+    author_email='christimperley@gmail.com',
+    url='https://github.com/ChrisTimperley/Bond',
+    license='Apache-2.0',
+    python_requires='>=3.5',
+    install_requires=[
+        'bugzoo>=2.1.11',
+        'rooibos>=0.3.0',
+        'attrs>=17.2.0',
+        'requests>=2.0.0'
+    ],
+    packages=['bond'],
+    package_dir={'': 'python'},
+    entry_points = {
+        'console_scripts': [ 'bond-test = bond.bond:test' ]
+    }
+)
