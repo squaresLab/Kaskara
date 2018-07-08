502b38d69a2ea228b2971f269a0f2bb85534c420
ChrisTimperley <christimperley@googlemail.com>
2018-07-07 20:45:28 -0400

    implemented build method for InsertionPointDB

--- python/kaskara/insertions.py
+++ python/kaskara/insertions.py
@@ -1,33 +1,69 @@
-from typing import FrozenSet, Iterable, Iterator
+__all__ = ['InsertionPointDB', 'InsertionPoint']
 
+from typing import FrozenSet, Iterable, Iterator, Dict, List
+import json
 import attr
-from bugzoo.core.location import FileLocationRange
+
+from bugzoo.client import Client as BugZooClient
+from bugzoo.core.bug import Bug as Snapshot
+from bugzoo.core.container import Container
+
+from .core import FileLocation
+from .exceptions import BondException
 
 
-@attr.s(frozen=True)
+@attr.s(frozen=True, repr=False)
 class InsertionPoint(object):
-    location = attr.ib(type=FileLocationRange)
+    location = attr.ib(type=FileLocation)
     visible = attr.ib(type=FrozenSet[str])
 
     @staticmethod
     def from_dict(d: Dict[str, str]) -> 'InsertionPoint':
-        location = FileLocationRange.from_string(d['location'])
+        location = FileLocation.from_string(d['location'])
         visible = frozenset(d['visible'])
         return InsertionPoint(location, visible)
+
+    def __repr__(self) -> str:
+        fmt = "InsertionPoint('{}', {{}})"
+        return fmt.format(self.location, ', '.join(self.visible))
 
     def to_dict(self) -> Dict[str, str]:
         return {'location': str(self.location),
                 'visible': [sym for sym in self.visible]}
 
 
-class InsertionPointDatabase(Iterable[InsertionPoint]):
+class InsertionPointDB(Iterable[InsertionPoint]):
     def __init__(self, contents: List[InsertionPoint]) -> None:
         self.__contents = contents
 
     @staticmethod
-    def from_dict(d: List[Dict[str, str]]) -> 'InsertionPointDatabase':
+    def build(client_bugzoo: BugZooClient,
+              snapshot: Snapshot,
+              files: List[str],
+              container: Container
+              ) -> 'InsertionPointDB':
+        out_fn = "insertion-points.json"
+        cmd = "kaskara-insertion-point-finder {}".format(' '.join(files))
+        workdir = "/ros_ws"
+        outcome = client_bugzoo.containers.exec(container,
+                                                cmd,
+                                                context=workdir)
+
+        print(outcome.output)
+
+        if outcome.code != 0:
+            msg = "kaskara-insertion-point-finder exited with non-zero code: {}"
+            msg = msg.format(outcome.code)
+            raise BondException(msg)
+
+        output = client_bugzoo.files.read(container, out_fn)
+        jsn = json.loads(output)  # type: List[Dict[str, str]]
+        return InsertionPointDB.from_dict(jsn)
+
+    @staticmethod
+    def from_dict(d: List[Dict[str, str]]) -> 'InsertionPointDB':
         contents = [InsertionPoint.from_dict(dd) for dd in d]
-        return InsertionPointDatabase(contents)
+        return InsertionPointDB(contents)
 
     def __iter__(self) -> Iterator[InsertionPoint]:
         yield from self.__contents
