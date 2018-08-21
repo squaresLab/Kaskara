4fd8c61a403a253dfdbe1377a5ab920ecb985628
Chris Timperley <christimperley@googlemail.com>
2018-08-21 16:55:19 -0400

    Fixed hard-coded paths to work with all BugZoo snapshots (#4)
    * improved logging and fixed hard-coded path
    
    * fixed hard-coded path in function scanner
    
    * fixed hard-coded path in insertion point finder

--- python/kaskara/loops.py
+++ python/kaskara/loops.py
@@ -1,6 +1,8 @@
 from typing import List, Dict
 import json
+import logging
 
+from bugzoo.util import indent
 from bugzoo.client import Client as BugZooClient
 from bugzoo.core.bug import Bug as Snapshot
 from bugzoo.core.container import Container
@@ -8,6 +10,9 @@ from bugzoo.core.container import Container
 from .core import FileLocationRange
 from .exceptions import BondException
 from .util import abs_to_rel_flocrange
+
+logger = logging.getLogger(__name__)  # type: logging.Logger
+logger.setLevel(logging.DEBUG)
 
 
 def find_loops(client_bugzoo: BugZooClient,
@@ -19,19 +24,24 @@ def find_loops(client_bugzoo: BugZooClient,
 
     out_fn = "loops.json"
     cmd = "kaskara-loop-finder {}".format(' '.join(files))
-    workdir = "/ros_ws"
+    workdir = snapshot.source_dir
+    logger.debug("executing loop finder [%s]: %s", workdir, cmd)
     outcome = client_bugzoo.containers.exec(container, cmd, context=workdir)
+    logger.debug("executed loop finder [%d]:\n%s",
+                 outcome.code, indent(outcome.output, 2))
 
     if outcome.code != 0:
         msg = "loop finder exited with non-zero code: {}"
         msg = msg.format(outcome.code)
         raise BondException(msg)
 
+    logger.debug("reading loop analysis results from file: %s", out_fn)
     output = client_bugzoo.files.read(container, out_fn)
     jsn = json.loads(output)  # type: List[Dict[str, str]]
     for loop_info in jsn:
         loc = FileLocationRange.from_string(loop_info['body'])
         loc = abs_to_rel_flocrange(snapshot.source_dir, loc)
         loop_bodies.append(loc)
+    logger.debug("finished reading loop analysis results")
 
     return loop_bodies
