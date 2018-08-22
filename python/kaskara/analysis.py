f6451b52d7174caf59e204246eb5451063a3b185
ChrisTimperley <christimperley@googlemail.com>
2018-08-21 22:58:33 -0400

    added from_dict and to_dict to Analysis

--- python/kaskara/analysis.py
+++ python/kaskara/analysis.py
@@ -1,6 +1,7 @@
 __all__ = ['Analysis']
 
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
+import json
 
 from bugzoo.core.bug import Bug as Snapshot
 from bugzoo.core.container import Container
@@ -13,6 +14,19 @@ from .insertions import InsertionPointDB
 
 
 class Analysis(object):
+    @staticmethod
+    def from_file(fn: str, snapshot: Snapshot) -> 'Analysis':
+        with open(fn, 'r') as f:
+            d = json.load(f)
+        return Analysis.from_dict(d, snapshot)
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any], snapshot: Snapshot) -> 'Analysis':
+        loop_bodies = [FileLocationRange.from_string(s) for s in d['loops']]
+        db_function = FunctionDB.from_dict(d['functions'], snapshot)
+        db_insertion = InsertionPointDB.from_dict(d['insertions'], snapshot)
+        return Analysis(loop_bodies, db_function, db_insertion)
+
     @staticmethod
     def build(client_bugzoo: BugZooClient,
               snapshot: Snapshot,
@@ -37,11 +51,20 @@ class Analysis(object):
                  loop_bodies: List[FileLocationRange],
                  db_function: FunctionDB,
                  db_insertion: InsertionPointDB
-
                  ) -> None:
         self.__location_bodies = FileLocationRangeSet(loop_bodies)
         self.__db_function = db_function
         self.__db_insertion = db_insertion
+
+    def to_dict(self, snapshot: Snapshot) -> Dict[str, Any]:
+        return {'functions': self.__db_function.to_dict(snapshot),
+                'insertions': self.__db_insertion.to_dict(snapshot),
+                'loops': [str(loc) for loc in self.__location_bodies]}
+
+    def to_file(self, fn: str, snapshot: Snapshot) -> None:
+        d = self.to_dict(snapshot)
+        with open(fn, 'w') as f:
+            json.dump(d, f)
 
     @property
     def functions(self) -> FunctionDB:
