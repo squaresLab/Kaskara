e16f225084eb338133c8646844e1a740df8439c7
ChrisTimperley <christimperley@googlemail.com>
2018-08-24 13:44:10 -0400

    added useful import

--- python/kaskara/__init__.py
+++ python/kaskara/__init__.py
@@ -4,6 +4,7 @@ from .version import __version__
 from .exceptions import BondException
 from .analysis import Analysis
 from .insertions import InsertionPoint
+from .statements import Statement
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
