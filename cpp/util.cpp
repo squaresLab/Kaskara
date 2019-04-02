f9635550a416d5972069fb7250812ef833eae9f4
ChrisTimperley <christimperley@googlemail.com>
2019-04-01 22:15:19 -0400

    add general (and simpler) fix to macro problem

--- cpp/util.cpp
+++ cpp/util.cpp
@@ -14,20 +14,22 @@ std::string const build_loc_str(clang::SourceRange const &range,
   clang::SourceManager const &SM = ctx->getSourceManager();
   clang::FullSourceLoc loc_begin = ctx->getFullLoc(range.getBegin());
   clang::FullSourceLoc loc_end = ctx->getFullLoc(range.getEnd());
+  clang::FileID file_id = SM.getDecomposedExpansionLoc(range.getBegin()).first;
+  std::string filename = SM.getFileEntryForID(file_id)->tryGetRealPathName();
 
   // if the starting location corresponds to a macro, we need to extract the
   // instantiation location for that macro (otherwise we'll end up with a
   // null pointer error later in the function).
   //
   // https://stackoverflow.com/questions/24062989/clang-fails-replacing-a-statement-if-it-contains-a-macro
-  if (loc_begin.isMacroID()) {
-    std::pair<clang::SourceLocation, clang::SourceLocation> expanded =
-      SM.getImmediateExpansionRange(loc_begin);
-    loc_begin = ctx->getFullLoc(expanded.first);
-  }
+  // if (loc_begin.isMacroID()) {
+  //   llvm::outs() << "start location is a macro expansion";
+  //   // std::pair<clang::SourceLocation, clang::SourceLocation> expanded =
+  //   //   SM.getImmediateExpansionRange(loc_begin);
+  //   clang::SourceLocation loc_ex = SM.getExpansionLoc(loc_begin);
+  //   loc_begin = ctx->getFullLoc(loc_begin);
+  // }
 
-  clang::FileID file_id = SM.getFileID(loc_begin);
-  std::string filename = SM.getFileEntryForID(file_id)->tryGetRealPathName();
   std::string s = fmt::format(fmt("{0}@{1}:{2}::{3}:{4}"),
                               filename,
                               loc_begin.getSpellingLineNumber(),
