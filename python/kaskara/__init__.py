77d2aba74e40095a41fae60b44172b05fe2e80a8
ChrisTimperley <christimperley@googlemail.com>
2020-01-13 13:03:56 +0800

    disable stdout logging by default

--- python/kaskara/__init__.py
+++ python/kaskara/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from loguru import logger as _logger
+
 from .version import __version__
 from .exceptions import KaskaraException
 from .project import Project
@@ -7,6 +9,7 @@ from .analyser import Analyser
 from .insertions import InsertionPoint
 from .statements import Statement
 from .loops import ProgramLoops
-
 from . import clang
 from . import python
+
+_logger.disable('kaskara')
