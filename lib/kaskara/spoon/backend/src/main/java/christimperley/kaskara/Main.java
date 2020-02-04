76da3920c2899b52ed9045d9f21a960ee14b234f
ChrisTimperley <christimperley@googlemail.com>
2020-02-04 13:29:18 -0500

    Added JSON encoding for statements

--- lib/kaskara/spoon/backend/src/main/java/christimperley/kaskara/Main.java
+++ lib/kaskara/spoon/backend/src/main/java/christimperley/kaskara/Main.java
@@ -1,5 +1,9 @@
 package christimperley.kaskara;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import spoon.Launcher;
@@ -13,7 +17,7 @@ public class Main {
      *
      * @param args  A list of command-line arguments.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var launcher = new Launcher();
         launcher.getEnvironment().setAutoImports(true);
         // add source code directories [specify as command line argument]
@@ -54,6 +58,13 @@ public class Main {
             var statement = Statement.forSpoonStatement(element);
             statements.add(statement);
             System.out.printf("%s%n%n", statement);
+        }
+
+        // TODO write to specified file
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        try (var fileOutputStream = new FileOutputStream("statements.json")) {
+            mapper.writeValue(fileOutputStream, statements);
         }
     }
 }
--- lib/kaskara/spoon/backend/src/main/java/christimperley/kaskara/Statement.java
+++ lib/kaskara/spoon/backend/src/main/java/christimperley/kaskara/Statement.java
@@ -1,12 +1,15 @@
 package christimperley.kaskara;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import spoon.reflect.code.CtStatement;
 import spoon.reflect.cu.SourcePosition;
+
 
 /**
  * Provides a summary of a single program statement.
  */
 public final class Statement {
+    private final Class kind;
     private final String source;
     private final SourcePosition position;
 
@@ -18,12 +21,49 @@ public final class Statement {
     public static Statement forSpoonStatement(CtStatement element) {
         var source = element.toString();
         var position = element.getPosition();
-        return new Statement(source, position);
+        var kind = element.getClass();
+        return new Statement(kind, source, position);
     }
 
-    public Statement(String source, SourcePosition position) {
+    /**
+     * Constructs a Statement description.
+     * @param kind      The class that represents the corresponding AST element for this statement.
+     * @param source    The original source code for the statement.
+     * @param position  The position of this statement.
+     */
+    public Statement(Class kind, String source, SourcePosition position) {
+        this.kind = kind;
         this.source = source;
         this.position = position;
+    }
+
+    @JsonGetter("kind")
+    public String getKind() {
+        return this.kind.getName();
+    }
+
+    @JsonGetter("source")
+    public String getSource() {
+        return this.source;
+    }
+
+    /**
+     * Returns the position of statement as a string.
+     * @return A string encoding of the position of the statement.
+     */
+    @JsonGetter("position")
+    public String getPositionAsString() {
+        var filename = this.position.getFile();
+        var startLine = this.position.getLine();
+        var startCol = this.position.getColumn();
+        var endLine = this.position.getEndLine();
+        var endCol = this.position.getEndColumn();
+        return String.format("%s@%d:%d::%d:%d",
+                this.position.getFile().getAbsolutePath(),
+                this.position.getLine(),
+                this.position.getColumn(),
+                this.position.getEndLine(),
+                this.position.getEndColumn());
     }
 
     @Override
