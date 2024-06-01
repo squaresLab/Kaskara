5350106d80e6f2bfb87efc171401412cd1cdef9b
ChrisTimperley <christimperley@googlemail.com>
2024-05-31 21:10:49 -0400

    added automatic volume creation for clang backend

--- src/kaskara/clang/post_install.py
+++ src/kaskara/clang/post_install.py
@@ -6,21 +6,87 @@ __all__ = ("post_install",)
 import contextlib
 
 import docker
+import docker.errors
 import pkg_resources
 from loguru import logger
 
 IMAGE_NAME: str = "christimperley/kaskara:cpp"
+VOLUME_NAME: str = "kaskara-clang"
+VOLUME_LOCATION: str = "/opt/kaskara"
+IMAGE_ID_LABEL: str = "kaskara.built-from-image-id"
+PLUGIN_LABEL: str = "kaskara.plugin"
+VERSION_LABEL: str = "kaskara.version"
+
+DEFAULT_KASKARA_DIRECTORY: str = "~/.kaskara"
 
 
-def post_install() -> None:
+def post_install(
+    kaskara_directory: str = DEFAULT_KASKARA_DIRECTORY,
+    *,
+    force: bool = False,
+) -> None:
     """Installs the C++ plugin backend."""
     logger.info("installing C++ plugin backend")
     backend_directory = pkg_resources.resource_filename(__name__, "backend")
-    logger.debug(f"backend located at: {backend_directory}")
+    kaskara_version = pkg_resources.get_distribution("kaskara").version
+    logger.trace(f"backend located at: {backend_directory}")
+
     with contextlib.closing(docker.from_env()) as docker_client:
-        logger.debug("prepared Docker client for C++ backend installation")
-        image, _ = docker_client.images.build(path=backend_directory,
-                                              tag=IMAGE_NAME,
-                                              pull=True)
-        logger.info(f"built Docker image for C++ plugin: {image.tags}")
+        logger.trace("prepared Docker client for C++ backend installation")
+
+        # does the image already exist?
+        image: docker.models.images.Image | None = None
+        try:
+            image = docker_client.images.get(IMAGE_NAME)
+        except docker.errors.ImageNotFound:
+            logger.info("C++ plugin Docker image doesn't exist")
+
+        image, _ = docker_client.images.build(
+            path=backend_directory,
+            tag=IMAGE_NAME,
+            pull=True,
+            labels={
+                PLUGIN_LABEL: "clang",
+                VERSION_LABEL: kaskara_version,
+            },
+        )
+
+        volume: docker.models.volumes.Volume | None = None
+        try:
+            volume = docker_client.volumes.get(VOLUME_NAME)
+        except docker.errors.NotFound:
+            logger.info("C++ plugin Docker volume doesn't exist")
+        else:
+            volume_labels = volume.attrs.get("Labels", {})
+            if volume_labels.get(IMAGE_ID_LABEL) != image.id:
+                logger.info("C++ plugin Docker volume is out-of-date")
+                volume.remove()
+                volume = None
+
+        if force or volume is None:
+            logger.info("creating C++ plugin Docker volume")
+
+            # create an empty volume
+            volume = docker_client.volumes.create(
+                name=VOLUME_NAME,
+                labels={
+                    PLUGIN_LABEL: "clang",
+                    IMAGE_ID_LABEL: image.id,
+                },
+            )
+
+            # create a container to populate the volume
+            assert isinstance(image.id, str)
+            docker_client.containers.run(
+                image=image.id,
+                command="true",
+                remove=True,
+                volumes={
+                    VOLUME_NAME: {
+                        "bind": VOLUME_LOCATION,
+                        "mode": "rw",
+                    },
+                },
+            )
+
     logger.info("installed C++ plugin backend")
